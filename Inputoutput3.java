ca58281f4798f60f4d3ae783302ee09a839e2f15
sriwahyuni-aj <ajsriwahyuni@gmail.com>
2017-03-01 22:43:43 +0700

    Add files via upload

---
+++ Inputoutput3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Inputoutput3{
+	public static void main(String[] args){
+
+Scanner input = new Scanner (System.in);
+String nama;
+nama =input.nextLine();
+
+	}
+}
